b3fca243544f713123113abf4706afe5a8ba11a1
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-07-09 12:05:39 +0800

    minor ui adjustments

--- resources/views/file-management/dashboard.blade.php
+++ resources/views/file-management/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('File Management Dashboard') }}
+                {{-- File Management Dashboard --}}
             </h2>
             <div class="flex space-x-2">
                 <a href="{{ route('files.index') }}"
